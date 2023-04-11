f330b22b7844d262aff7b8456beea9e8c7ab1cd2
statictaku <statictaku@gmail.com>
2023-04-11 21:58:12 +0900

    RelU, SoftMaxWithLoss passed test. Affine.backward still left

--- cuda_env/code/main.cu
+++ cuda_env/code/main.cu
@@ -1,6 +1,7 @@
 #include <matrix.hpp>
 #include <curand.h>
 #include <curand_kernel.h>
+#include <math.h>
 
 using namespace cuda_Matrix;
 
@@ -20,7 +21,7 @@ void generate_normal_weights(Matrix* _weight, int seed)
 __global__ 
 void initialize_bias(Matrix* _bias, int seed)
 {
-    int id  = blockIdx.x * blockDim.x + threadIdx.x;;
+    int id  = blockIdx.x * blockDim.x + threadIdx.x;
     if (id < _bias->size)
         _bias->elements[id] = id;
 }
@@ -28,9 +29,180 @@ void initialize_bias(Matrix* _bias, int seed)
 __global__ 
 void set_one_vector(Matrix* _bias, int seed)
 {
-    int id  = blockIdx.x * blockDim.x + threadIdx.x;;
+    int id  = blockIdx.x * blockDim.x + threadIdx.x;
     if (id < _bias->size)
         _bias->elements[id] = 1;
+}
+
+__global__
+void set_sum_value_par_col(const Matrix* x, Matrix* max_value, int two_n)
+{
+    extern __shared__ float sdata[];//blockDim.x * blockDim.y * sizeof(float)
+    int id_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int id_y = threadIdx.y;
+
+    int sid_x = threadIdx.x;
+    int sid_y = threadIdx.y;
+
+    if(id_x < x->width)
+    {
+        sdata[sid_y * blockDim.x + sid_x] = x->elements[id_y * x->width + id_x];
+    }
+    __syncthreads();
+
+    for(int s=1; s< two_n; s*=2)
+    {
+        if(id_x < x->width && id_y % (2*s) == 0 && id_y < two_n)
+        {
+            //if(id_x == 0 && s == 8)
+            //    printf("%d,%d, %f, %f,8\n",sid_y, sid_y+s, sdata[sid_y * blockDim.x + sid_x], sdata[(sid_y + s)* blockDim.x + sid_x]);
+            sdata[sid_y * blockDim.x + sid_x] += sdata[(sid_y + s)* blockDim.x + sid_x];
+            //if(id_x == 0 && s == 1)
+            //    printf("%d, %f, 1\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+            //if(id_x == 0 && s == 4)
+            //    printf("%d, %f,2\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+        }
+        __syncthreads();
+    }
+
+    if(id_y == 0 && id_x < x->width)
+    {
+        max_value->elements[id_x] = sdata[sid_x];
+        for(int id = two_n; id<x->height; ++id)
+            max_value->elements[id_x] += sdata[id * blockDim.x + sid_x];
+    }
+
+}
+
+__global__
+void set_max_value_par_col(const Matrix* x, Matrix* max_value, int two_n)
+{
+    extern __shared__ float sdata[];//blockDim.x * blockDim.y * sizeof(float)
+    int id_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int id_y = threadIdx.y;
+
+    int sid_x = threadIdx.x;
+    int sid_y = threadIdx.y;
+
+    if(id_x < x->width)
+    {
+        sdata[sid_y * blockDim.x + sid_x] = x->elements[id_y * x->width + id_x];
+    }
+    __syncthreads();
+
+    for(int s=1; s< two_n; s*=2)
+    {
+        if(id_x < x->width && id_y % (2*s) == 0 && id_y < two_n)
+        {
+            //if(id_x == 0 && s == 8)
+            //    printf("%d,%d, %f, %f,8\n",sid_y, sid_y+s, sdata[sid_y * blockDim.x + sid_x], sdata[(sid_y + s)* blockDim.x + sid_x]);
+            sdata[sid_y * blockDim.x + sid_x] = sdata[sid_y * blockDim.x + sid_x] > sdata[(sid_y + s)* blockDim.x + sid_x] ? sdata[sid_y * blockDim.x + sid_x]:sdata[(sid_y + s)* blockDim.x + sid_x];
+            //if(id_x == 0 && s == 1)
+            //    printf("%d, %f, 1\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+            //if(id_x == 0 && s == 4)
+            //    printf("%d, %f,2\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+        }
+        __syncthreads();
+    }
+
+    if(id_y == 0 && id_x < x->width)
+    {
+        max_value->elements[id_x] = sdata[sid_x];
+        for(int id = two_n; id<x->height; ++id)
+            max_value->elements[id_x] = max_value->elements[id_x] > sdata[id * blockDim.x + sid_x] ? max_value->elements[id_x]:sdata[id * blockDim.x + sid_x];
+    }
+
+}
+
+__global__
+void kernel_softmax(const Matrix* x, Matrix* sum_value, Matrix* max_value, Matrix* output, int two_n)
+{
+    extern __shared__ float sdata[];//blockDim.x * blockDim.y * sizeof(float)
+
+    int id_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int id_y = threadIdx.y;
+
+    int sid_x = threadIdx.x;
+    int sid_y = threadIdx.y;
+
+    if(id_x < x->width)
+    {
+        //printf("%d,%d, %f,%f,%f,%f\n",id_y, id_x,x->elements[id_y * x->width + id_x], expf(x->elements[id_y * x->width + id_x]), expf(x->elements[id_y * x->width + id_x]-max_value->elements[id_x]), max_value->elements[id_x]);
+        sdata[sid_y * blockDim.x + sid_x] = expf(x->elements[id_y * x->width + id_x]-max_value->elements[id_x]);
+    }
+    __syncthreads();
+
+    for(int s=1; s< two_n; s*=2)
+    {
+        if(id_x < x->width && id_y % (2*s) == 0 && id_y < two_n)
+        {
+            //if(id_x == 0 && s == 1)
+            //    printf("%d,%d, %f, %f,1\n",sid_y, sid_y+s, sdata[sid_y * blockDim.x + sid_x], sdata[(sid_y + s)* blockDim.x + sid_x]);
+            //if(id_x == 0 && s == 8)
+            //    printf("%d,%d, %f, %f,8\n",sid_y, sid_y+s, sdata[sid_y * blockDim.x + sid_x], sdata[(sid_y + s)* blockDim.x + sid_x]);
+            sdata[sid_y * blockDim.x + sid_x] += sdata[(sid_y + s)* blockDim.x + sid_x];
+            //if(id_x == 0 && s == 1)
+            //    printf("%d, %f, 1\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+            //if(id_x == 0 && s == 4)
+            //    printf("%d, %f,4\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+        }
+        __syncthreads();
+    }
+
+    if(id_y == 0 && id_x < x->width)
+    {
+        sum_value->elements[id_x] = sdata[sid_x];
+        for(int id = two_n; id<x->height; ++id)
+        {
+            sum_value->elements[id_x] += sdata[id * blockDim.x + sid_x];
+            //printf("%f,%f\n",sum_value->elements[id_x], sdata[id * blockDim.x + sid_x]);
+        }
+    }
+    __syncthreads();
+    if(id_x < x->width)
+    {
+        output->elements[id_y * x->width + id_x] = expf(x->elements[id_y * x->width + id_x]-max_value->elements[id_x])/sum_value->elements[id_x];
+        //printf("%d, %d, %f, %f, %f\n",id_y, id_x, x->elements[id_y * x->width + id_x]-max_value->elements[id_x], expf(x->elements[id_y * x->width + id_x]-max_value->elements[id_x])/sum_value->elements[id_x], output->elements[id_y * x->width + id_x]);
+    }
+}
+
+__global__
+void kernel_cross_entropy(const Matrix* x, const Matrix* t, Matrix* E, int two_n)
+{
+    extern __shared__ float sdata[];//blockDim.x * blockDim.y * sizeof(float)
+    int id_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int id_y = threadIdx.y;
+
+    int sid_x = threadIdx.x;
+    int sid_y = threadIdx.y;
+
+    if(id_x < x->width)
+    {
+        sdata[sid_y * blockDim.x + sid_x] = -t->elements[id_y * x->width + id_x] * logf(x->elements[id_y * x->width + id_x]);
+    }
+    __syncthreads();
+
+    for(int s=1; s< two_n; s*=2)
+    {
+        if(id_x < x->width && id_y % (2*s) == 0 && id_y < two_n)
+        {
+            //if(id_x == 0 && s == 8)
+            //    printf("%d,%d, %f, %f,8\n",sid_y, sid_y+s, sdata[sid_y * blockDim.x + sid_x], sdata[(sid_y + s)* blockDim.x + sid_x]);
+            sdata[sid_y * blockDim.x + sid_x] += sdata[(sid_y + s)* blockDim.x + sid_x];
+            //if(id_x == 0 && s == 1)
+            //    printf("%d, %f, 1\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+            //if(id_x == 0 && s == 4)
+            //    printf("%d, %f,2\n",sid_y, sdata[sid_y * blockDim.x + sid_x]);
+        }
+        __syncthreads();
+    }
+
+    if(id_y == 0 && id_x < x->width)
+    {
+        E->elements[id_x] = sdata[sid_x];
+        for(int id = two_n; id<x->height; ++id)
+            E->elements[id_x] += sdata[id * blockDim.x + sid_x];
+    }
 }
 
 struct Affine
@@ -135,7 +307,9 @@ struct SoftMaxWithLoss//lastlayer
     Matrix y;
     Matrix t;
     Matrix loss;
-    SoftMaxWithLoss(int _input_size, int _output_size, int _batch_size, bool _onGpu = false):
+    int input_size, output_size, batch_size;
+    bool onGpu;
+    SoftMaxWithLoss(int _input_size, int _output_size, int _batch_size, bool _onGpu = false):input_size(_input_size), output_size(_output_size), batch_size(_batch_size),onGpu(_onGpu),
     y(_batch_size, _output_size, _onGpu),
     t(_batch_size, _output_size, _onGpu),
     loss(_batch_size, 1, _onGpu)
@@ -143,10 +317,10 @@ struct SoftMaxWithLoss//lastlayer
 
     };
 
-    /*Matrix forward(const Matrix& x, const Matrix& _t)
+    Matrix forward(const Matrix& x, const Matrix& _t)
     {
         t = _t;
-        Matrix output = activate(x, y);//softmax
+        y = softmax(x);//softmax　各列を1個の入力としてsoftmaxをかける
         loss = cross_entropy_error(y, t);
 
         return loss;
@@ -156,55 +330,87 @@ struct SoftMaxWithLoss//lastlayer
     {
         Matrix output = Matrix::Sub(y, t);
         return output;
-    }*/
-};
-
-
-//Affin - RelU - Affine - SoftMaxWithLoss
-//SoftMaxWithLoss.y = probability of each number
-//
-/*int main() 
-{
-    int input_size = 12*20;
-    int hidden_size = 50;
-    int batch_size = 2*16;
-    bool onGPU = true;
-    Affine Layer1(input_size, hidden_size, batch_size, 0.01, onGPU);
-    Matrix h_x(batch_size, input_size);
-    Matrix d_x(batch_size, input_size, onGPU);
-
-    for(int id = 0;id<h_x.size;++id)
-        h_x.elements[id] = 1;
-    
-    Matrix test(batch_size, hidden_size);
-    d_x = h_x;
-    //test = Layer1.forward(d_x);
-    test = Matrix::Mul( Layer1.weight, d_x);
-    Matrix tt(input_size, hidden_size);
-    tt = Layer1.weight;
-
-    
-    for(int row = 0;row<test.height;++row)
-    {
-        for(int column = 0;column<test.width;++column)
-        {
-            if(test.elements[row*test.width + column] != 12*20 )
-            {
-                std::cout << row << " " << column << " " << test.elements[row*test.width + column] << std::endl;
-                std::exit(1);
-            }
-        }
     }
-    return 0;
 
-    for(int id = 0;id<tt.size;++id)
-        if(tt.elements[id] != 1)
-            std::cout << tt.elements[id] << std::endl;
 
-    for(int id = 0;id<h_x.size;++id)
-        if(h_x.elements[id] != 1)
-            std::cout << h_x.elements[id] << std::endl;
-}*/
+    //todo: measure for overflow
+    //sum
+    Matrix get_sum_value_par_col(const Matrix& x)
+    {
+        Matrix max_value(x.width, 1, x.onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        dim3 dimBlock(BLOCK_SIZE,x.height);
+        dim3 dimGrid ((x.width + dimBlock.x - 1) / dimBlock.x);
+        int two_n = 1;
+
+        while(two_n * 2 <= x.height){
+            two_n *= 2;
+        }
+        set_sum_value_par_col<<< dimGrid, dimBlock, dimBlock.x*dimBlock.y*sizeof(float) >>>(devPtr<Matrix>(x).GetDevPtr(), devPtr<Matrix>(max_value).GetDevPtr(), two_n);
+        cudaDeviceSynchronize();
+        return max_value;
+    }
+
+    //todo: measure for overflow
+    Matrix softmax(const Matrix& x)
+    {
+        Matrix sum_value(x.width, 1, x.onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        Matrix max_value(x.width, 1, x.onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        Matrix output (x.width, x.height, x.onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        
+        dim3 dimBlock(BLOCK_SIZE,x.height);
+        dim3 dimGrid ((x.width + dimBlock.x - 1) / dimBlock.x);
+        int two_n = 1;
+
+        while(two_n * 2 <= x.height){
+            two_n *= 2;
+        }
+        max_value = get_max_value_par_col(x);
+        kernel_softmax<<< dimGrid, dimBlock, dimBlock.x*dimBlock.y*sizeof(float) >>>(devPtr<Matrix>(x).GetDevPtr(), 
+        devPtr<Matrix>(sum_value).GetDevPtr(), 
+        devPtr<Matrix>(max_value).GetDevPtr(),  
+        devPtr<Matrix>(output).GetDevPtr(),  
+        two_n);
+        cudaDeviceSynchronize();
+        return output;
+    }
+
+    Matrix cross_entropy_error(const Matrix& y_, const Matrix& t_)
+    {
+        Matrix E (y_.width, 1, onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        
+        dim3 dimBlock(BLOCK_SIZE,y_.height);
+        dim3 dimGrid ((y_.width + dimBlock.x - 1) / dimBlock.x);
+        int two_n = 1;
+
+        while(two_n * 2 <= y_.height){
+            two_n *= 2;
+        }
+        kernel_cross_entropy<<< dimGrid, dimBlock, dimBlock.x*dimBlock.y*sizeof(float) >>>(
+        devPtr<Matrix>(y_).GetDevPtr(), 
+        devPtr<Matrix>(t_).GetDevPtr(), 
+        devPtr<Matrix>(E).GetDevPtr(),  
+        two_n);
+        cudaDeviceSynchronize();
+        return E;
+    }
+
+    //max
+    Matrix get_max_value_par_col(const Matrix& x)
+    {
+        Matrix max_value(x.width, 1, x.onGpu);//max_value[col] = max(x[0][col],x[1][col], , ,)
+        dim3 dimBlock(BLOCK_SIZE,x.height);
+        dim3 dimGrid ((x.width + dimBlock.x - 1) / dimBlock.x);
+        int two_n = 1;
+
+        while(two_n * 2 <= x.height){
+            two_n *= 2;
+        }
+        set_max_value_par_col<<< dimGrid, dimBlock, dimBlock.x*dimBlock.y*sizeof(float) >>>(devPtr<Matrix>(x).GetDevPtr(), devPtr<Matrix>(max_value).GetDevPtr(), two_n);
+        cudaDeviceSynchronize();
+        return max_value;
+    }
+
+};
 
 int main()
 {
@@ -274,6 +480,8 @@ int main()
             }
         }
 
+        Layer1.backward();
+
     }
 
     {
@@ -334,6 +542,92 @@ int main()
                     assert(h_y2.elements[row*h_x.width + column] == 0);
                 else if(h_x.elements[row*h_x.width + column] > 0)
                     assert(h_y2.elements[row*h_x.width + column] == -1);
+            }
+        }
+
+    }
+
+    {
+        int input_size = 50;
+        int output_size = 10;
+        int batch_size = 100;
+        SoftMaxWithLoss Layer3(input_size, output_size, batch_size, true);
+        Matrix h_x(batch_size, output_size);
+        Matrix d_x(batch_size, output_size, true);
+
+        Matrix h_y(batch_size, 1);
+        Matrix h_y_soft(batch_size, output_size);
+        Matrix h_y_loss(batch_size, 1);
+        for(int row = 0;row<h_x.height;++row)
+        {
+            for(int col = 0;col<h_x.width;++col)
+            {
+                h_x.elements[row * h_x.width + col] = row;
+            }
+        }
+        d_x = h_x;
+        h_y = Layer3.get_sum_value_par_col(d_x);
+
+        for(int col = 0;col<h_y.width;++col)
+        {
+            assert(h_y.elements[col] == 0.5*output_size*(output_size-1));
+        }
+
+        h_y = Layer3.get_max_value_par_col(d_x);
+
+        for(int col = 0;col<h_y.width;++col)
+        {
+            assert(h_y.elements[col] == (output_size-1));
+        }
+
+        h_y_soft = Layer3.softmax(d_x);
+
+        for(int row = 0;row<h_y_soft.height;++row)
+        {
+            for(int col = 0;col<h_y_soft.width;++col)
+            {
+                //printf("%d,%d,%f\n", row, col, h_y_soft.elements[row * h_y_soft.width + col]);
+            }
+        }
+
+        Matrix h_teacher(batch_size, output_size);
+        Matrix d_teacher(batch_size, output_size, true);
+
+
+        int ans = 9;
+
+
+
+        for(int row = 0;row<h_teacher.height;++row)
+        {
+            for(int col = 0;col<h_teacher.width;++col)
+            {
+                h_teacher.elements[row * h_x.width + col] = row ==  ans? 1:0;
+            }
+        }
+
+        d_teacher = h_teacher;
+        h_y_loss = Layer3.forward(d_x, d_teacher);
+
+        float exp_sum = 0;
+        for(int i = 0;i<output_size;++i)
+        {
+            exp_sum += std::exp(i);
+        }
+        
+        for(int col = 0;col<h_y_loss.width;++col)
+        {
+            //std::cout << -std::log(std::exp(ans)/exp_sum) << " " << h_y_loss.elements[col] << std::endl;
+        }
+
+        Matrix ans_x(batch_size, output_size);
+        ans_x = Layer3.backward(d_x);
+
+        for(int row = 0;row<h_teacher.height;++row)
+        {
+            for(int col = 0;col<h_teacher.width;++col)
+            {
+                printf("%d,%d,%f,%f\n", row, col, 100*ans_x.elements[row * h_x.width + col], h_y_soft.elements[row * h_x.width + col] - h_teacher.elements[row * h_x.width + col]);
             }
         }
 
